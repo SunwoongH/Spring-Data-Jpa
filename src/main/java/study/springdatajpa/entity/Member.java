4ca6845974120dcb169e0a69902c9140b1dec9e5
sunwoong <sunwoong1122@gmail.com>
2023-07-05 18:38:41 +0900

    feat: 회원 엔티티 구현

---
+++ src/main/java/study/springdatajpa/entity/Member.java
@@ -0,0 +1,35 @@
+package study.springdatajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@ToString(of = {"id", "username", "age"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    @Builder
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
