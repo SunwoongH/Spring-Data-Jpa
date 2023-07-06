e875330296383318adc3c4c122ef7f63950efe91
sunwoong <sunwoong1122@gmail.com>
2023-07-06 16:26:04 +0900

    test: 회원 repository 테스트 코드 작성

---
+++ src/test/java/study/springdatajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,79 @@
+package study.springdatajpa.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.springdatajpa.entity.Member;
+import study.springdatajpa.entity.Team;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Transactional
+@SpringBootTest
+class MemberJpaRepositoryTest {
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @DisplayName("회원을 저장한다.")
+    @Test
+    void saveTest() {
+        // given
+        Member member = createMember("memberA", 25, null);
+
+        // when
+        Member savedMember = memberJpaRepository.save(member);
+
+        // then
+        Member findMember = memberJpaRepository.findById(savedMember.getId())
+                .orElseThrow(() -> new IllegalArgumentException("id: " + savedMember.getId() + " 회원은 존재하지 않습니다."));
+        assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+        assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
+        assertThat(findMember.getAge()).isEqualTo(savedMember.getAge());
+        assertThat(findMember.getTeam()).isEqualTo(savedMember.getTeam());
+    }
+
+    @DisplayName("회원을 삭제한다.")
+    @Test
+    void deleteTest() {
+        // given
+        Member member = createMember("memberA", 25, null);
+        Member savedMember = memberJpaRepository.save(member);
+
+        // when
+        memberJpaRepository.delete(member);
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> memberJpaRepository.findById(savedMember.getId())
+                .orElseThrow(() -> new IllegalArgumentException("id: " + savedMember.getId() + " 회원은 존재하지 않습니다.")));
+    }
+
+    @DisplayName("회원들을 조회한다.")
+    @Test
+    void findAllTest() {
+        // given
+        final int memberCount = 2;
+        Member memberA = createMember("memberA", 25, null);
+        Member memberB = createMember("memberB", 25, null);
+        memberJpaRepository.save(memberA);
+        memberJpaRepository.save(memberB);
+
+        // when
+        List<Member> members = memberJpaRepository.findAll();
+
+        // then
+        assertThat(members.size()).isEqualTo(memberCount);
+    }
+
+    private Member createMember(String username, int age, Team team) {
+        return Member.builder()
+                .username(username)
+                .age(age)
+                .team(team)
+                .build();
+    }
+}
