ef6b2a2de07f1bfe0a1b6654a922fc650b5f9b80
sunwoong <sunwoong1122@gmail.com>
2023-07-07 17:22:38 +0900

    feat: 순수 JPA 회원 조회 기능 추가

--- src/main/java/study/springdatajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/springdatajpa/repository/MemberJpaRepository.java
@@ -35,4 +35,31 @@ public class MemberJpaRepository {
         return entityManager.createQuery("select count(m) from Member m", Long.class)
                 .getSingleResult();
     }
+
+    public List<Member> findByUsernameAndAgeGreaterThan(String username, int age) {
+        return entityManager.createQuery("select m from Member m where m.username = :username and m.age > :age", Member.class)
+                .setParameter("username", username)
+                .setParameter("age", age)
+                .getResultList();
+    }
+
+    public List<Member> findByUsername(String username) {
+        return entityManager.createNamedQuery("Member.findByUsername", Member.class)
+                .setParameter("username", username)
+                .getResultList();
+    }
+
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return entityManager.createQuery("select m from Member m where m.age = :age order by m.username desc", Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return entityManager.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
